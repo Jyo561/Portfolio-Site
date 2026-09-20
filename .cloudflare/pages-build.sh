f38b44a06cb0340c2155a23275669d1da3a71f04
Jyo561 <jyotiradityakuanar34@gmail.com>
2026-09-20 08:45:12 +0530

    Added Data

--- .cloudflare/pages-build.sh
+++ .cloudflare/pages-build.sh
@@ -8,8 +8,15 @@ source $HOME/.cargo/env
 # Add wasm target
 rustup target add wasm32-unknown-unknown
 
-# Install trunk
-cargo install trunk
+# Install Trunk from the prebuilt release binary
+curl -L \
+  https://github.com/trunk-rs/trunk/releases/download/v0.21.14/trunk-x86_64-unknown-linux-gnu.tar.gz \
+  -o /tmp/trunk.tar.gz
+
+tar -xzf /tmp/trunk.tar.gz -C /tmp
+
+chmod +x /tmp/trunk
+mv /tmp/trunk "$HOME/.cargo/bin/trunk"
 
 # Build the project
 trunk build --release
